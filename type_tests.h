9943a9336afc1903fa899bbd73f6d20cf01b82dd
Jeroen De Busser <jeroendebusser@telenet.be>
2016-11-29 21:47:20 +0100

    Added type_tests

---
+++ type_tests.h
@@ -0,0 +1,18 @@
+#include <type_traits>
+
+template<typename... T>
+struct any_of;
+
+template<typename H, typename... T>
+struct any_of<H,T...> {
+  template<typename U>
+  struct apply {
+    static constexpr bool value = std::is_same<H,U>::value || any_of<T...>::template apply<U>::value;
+  };
+};
+
+template<>
+struct any_of<> {
+  template<typename U>
+  struct apply : std::false_type {};
+};
