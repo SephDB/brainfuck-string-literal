f07e39257850247c298049c839f34f5a10bc2022
Seph De Busser <seph.debusser@gmail.com>
2020-08-08 14:56:20 +0200

    Added generic any_of with type tests

--- type_tests.h
+++ type_tests.h
@@ -20,4 +20,21 @@ struct any_of<> {
   struct apply : std::false_type {};
 };
 
+template<template<typename> class... Tests>
+struct any_of_tests;
+
+template<template<typename> class H, template<typename> class... T>
+struct any_of_tests<H,T...> {
+  template<typename U>
+  struct apply {
+    static constexpr bool value = H<U>::value || any_of_tests<T...>::template apply<U>::value;
+  };
+};
+
+template<>
+struct any_of_tests<> {
+  template<typename U>
+  struct apply : std::false_type {};
+};
+
 #endif
